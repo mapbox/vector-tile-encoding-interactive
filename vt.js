313782a5be7825d3ec90a2fd0a74b8169acea5a0
mapsam <matthews.sam@gmail.com>
2016-03-07 12:26:40 -0800

    update steps, styles

--- vt.js
+++ vt.js
@@ -1,15 +1,16 @@
-var cellSize = 25;
-var tilesize = 10;
-var padding = 1;
-var buffer = 2;
-var buffx = padding + (buffer * cellSize);
-var stepCount = 0;
-var btn;
-var title = document.getElementById('vt-title');
-var desc = document.getElementById('vt-description');
-var command = document.getElementById('vt-command');
-var ctx;
-var grid;
+var cellSize = 25,
+    tilesize = 12,
+    padding = 1,
+    buffer = 2,
+    buffx = padding + (buffer * cellSize),
+    stepCount = 0,
+    btn,
+    title = document.getElementById('vt-title'),
+    desc = document.getElementById('vt-description'),
+    command = document.getElementById('vt-command'),
+    commandSteps = document.getElementById('vt-command-steps'),
+    ctx,
+    grid;
 
 window.onload = function() {
   grid = document.getElementById('grid');
@@ -31,40 +32,51 @@ function makeGrid(canvas, size, buffer) {
   drawGrid(ctx, size, buffer);
 }
 
-function drawGrid(context, size, buff) {
-  for (x = padding; x <= context.canvas.width + padding; x+=cellSize) {
-    for (y = padding; y <= context.canvas.height + padding; y+=cellSize) {
+function drawGrid(c, size, buff) {
+  for (x = padding; x <= c.canvas.width + padding; x+=cellSize) {
+    for (y = padding; y <= c.canvas.height + padding; y+=cellSize) {
       if (x < (padding + buff * cellSize) ||
           y < (padding + buff * cellSize) ||
           x-cellSize > ((cellSize * size + (padding * 2)) - (buff * cellSize)) ||
           y-cellSize > ((cellSize * size + (padding * 2)) - (buff * cellSize))) {
-        context.fillStyle='#e5e5e5';
-        context.fillRect(x-0.5, y-0.5, cellSize, cellSize);
+        c.fillStyle='#e5e5e5';
+        c.fillRect(x-0.5, y-0.5, cellSize, cellSize);
       }
 
-      context.strokeStyle='#666666';
-      context.strokeRect(x-0.5, y-0.5, cellSize, cellSize);
+      c.strokeStyle='#666666';
+      c.strokeRect(x-0.5, y-0.5, cellSize, cellSize);
     }
   }
 }
 
 function next(index) {
+  var s = steps[index];
   try {
-    title.innerHTML = steps[index].name;
-    desc.innerHTML = steps[index].description;
-    command.innerHTML = steps[index].commandx;
-    btn.innerHTML = 'Next step';
-    steps[index].command();
+    title.innerHTML = s.name;
+    desc.innerHTML = s.description;
+    command.innerHTML = s.commandx;
+    btn.innerHTML = s.buttonText;
+    s.command();
+    addStep(s.commandx, s.color);
     stepCount++;
   } catch (err) {
-    title.innerHTML = 'Back at the beginning';
-    desc.innerHTML = 'Woop';
-    command.innerHTML = 'A command';
-    btn.innerHTML = 'Start over';
+    title.innerHTML = 'Step 0';
+    desc.innerHTML = 'The vector tile to the left is a 10x10 grid with 2 cell buffer. Let\'s encode some geometry to the grid, starting with a <span class="poly blue">blue polygon</span>.';
+    command.innerHTML = 'An empty vecotor tile';
+    btn.innerHTML = 'Next step';
+    commandSteps.innerHTML = '';
     ctx.clearRect(0, 0, grid.width, grid.height);
     makeGrid(grid, tilesize, buffer);
     stepCount=0;
   }
+}
+
+function addStep(cmd, color) {
+  var elem = document.createElement('div');
+  elem.className = 'command-step';
+  elem.style.color = color;
+  elem.innerHTML = cmd;
+  commandSteps.appendChild(elem);
 }
 
 var cmd = {
@@ -82,10 +94,12 @@ var cmd = {
 var steps = [
   {
     "name": "Step 1",
-    "description": "The first action when encoding a polygon is to point the command to a starting point. This uses the <code>MoveTo(x,y)</code> command. The blue dot is at <code>2, 2</code> starting at the top left of the grid.",
+    "color": "#0074D9",
+    "description": "The first action when encoding a polygon is to point the command to a starting point. This uses the <code>MoveTo(x,y)</code> command. The <span class='poly blue'>blue</span> dot is at <code>2, 2</code> starting at the top left of the grid.",
     "commandx": "MoveTo(1,2)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.fillStyle = 'blue';
+      ctx.fillStyle = '#0074D9';
       ctx.beginPath();
       cmd.arc(1,2);
       ctx.fill();
@@ -93,10 +107,12 @@ var steps = [
   },
   {
     "name": "Step 2",
+    "color": "#0074D9",
     "description": "In order to move from a starting position, we use a <code>LineTo(x,y)</code> command. The X and Y values are relative to the previous command, rather than the origin of the grid, to keep file size down.",
     "commandx": "LineTo(3,-1)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'blue';
+      ctx.strokeStyle = '#0074D9';
       ctx.beginPath();
       cmd.mt(1,2);
       cmd.lt(4,1);
@@ -105,10 +121,12 @@ var steps = [
   },
   {
     "name": "Step 3",
-    "description": "Drawing another path in the triangle polygon.",
+    "color": "#0074D9",
+    "description": "Drawing another path of the <span class='poly blue'>blue polygon</span>.",
     "commandx": "LineTo(3,4)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'blue';
+      ctx.strokeStyle = '#0074D9';
       ctx.beginPath();
       cmd.mt(4,1);
       cmd.lt(7,5);
@@ -117,10 +135,12 @@ var steps = [
   },
   {
     "name": "Step 4",
-    "description": "Drawing another path in the blue polygon.",
+    "color": "#0074D9",
+    "description": "Drawing another path of the <span class='poly blue'>blue polygon</span>.",
     "commandx": "LineTo(-4,2)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'blue';
+      ctx.strokeStyle = '#0074D9';
       ctx.beginPath();
       cmd.mt(7,5);
       cmd.lt(3,7);
@@ -129,10 +149,12 @@ var steps = [
   },
   {
     "name": "Step 5",
+    "color": "#0074D9",
     "description": "Finally, we close a path. This uses the <code>ClosePath()</code> command that closes the path to most recently used <code>MoveTo(x,y)</code> command, which is our starting point.",
     "commandx": "ClosePath()",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'blue';
+      ctx.strokeStyle = '#0074D9';
       ctx.beginPath();
       cmd.mt(3,7);
       cmd.lt(1,2);
@@ -141,10 +163,12 @@ var steps = [
   },
   {
     "name": "Step 6",
-    "description": "Now on to the <span style='color: red'>red polygon</span>! This requires another <code>MoveTo</code> command relative to the last <code>LineTo</code> of the previous polygon.",
+    "color": "#FF4136",
+    "description": "Now on to the <span class='poly red'>red polygon</span>. This requires another <code>MoveTo</code> command relative to the last <code>LineTo</code> of the previous polygon.",
     "commandx": "MoveTo(1,-5)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.fillStyle = 'red';
+      ctx.fillStyle = '#FF4136';
       ctx.beginPath();
       cmd.arc(4,2);
       ctx.fill();
@@ -152,10 +176,12 @@ var steps = [
   },
   {
     "name": "Step 7",
-    "description": "Since the <span style='color: red'>red polygon</span> is an ",
+    "color": "#FF4136",
+    "description": "Since the <span class='poly red'>red polygon</span> is technically a \"hole\" of the <span class='poly blue'>blue polygon</span> it is considered an <em>interior ring</em>. The Mapbox Vector Tile Specification requires this interior ring to be drawn counter-clockwise, opposite of the <span class='poly blue'>blue polygon</span>.",
     "commandx": "LineTo(-1,2)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'red';
+      ctx.strokeStyle = '#FF4136';
       ctx.beginPath();
       cmd.mt(4,2);
       cmd.lt(3,4);
@@ -164,10 +190,12 @@ var steps = [
   },
   {
     "name": "Step 7",
-    "description": "Since the <span style='color: red'>red polygon</span> is an ",
+    "color": "#FF4136",
+    "description": "Another addition to the <span class='poly red'>red polygon</span>.",
     "commandx": "LineTo(2,1)",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'red';
+      ctx.strokeStyle = '#FF4136';
       ctx.beginPath();
       cmd.mt(3,4);
       cmd.lt(5,5);
@@ -176,14 +204,24 @@ var steps = [
   },
   {
     "name": "Step 7",
-    "description": "Since the <span style='color: red'>red polygon</span> is an ",
+    "color": "#FF4136",
+    "description": "Finally, we close the <span class='poly red'>red polygon</span> by drawing back to the most recent <code>MoveTo()</code> command.",
     "commandx": "ClosePath()",
+    "buttonText": "Next step",
     "command": function() {
-      ctx.strokeStyle = 'red';
+      ctx.strokeStyle = '#FF4136';
       ctx.beginPath();
       cmd.mt(5,5);
       cmd.lt(4,2);
       ctx.stroke();
     }
-  }
+  },
+  {
+    "name": "Fin.",
+    "color": "#FF4136",
+    "description": "Encoding a single vector tile into <code>.mvt</code> format happens quickly with tools like Mapnik or Node Mapnik. It's important to keep in mind that geometry coordinates are translated into non-geographic vector grid coordiantes, which results in some simplification. In order to reduce the visual impact, vector tiles are rendered to a maximum zoom level. Once you hit that zoom level, another tile is loaded with more detail.",
+    "commandx": "",
+    "buttonText": "Start over",
+    "command": function() {}
+  },
 ];
